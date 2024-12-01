e0880efbc358225d56cc23d47155f7b89a548df3
Slobich <slobich@gmail.com>
2024-12-01 10:28:49 +0100

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://slobich.github.io/slobich-graphics',
+  site: 'https://slobich.github.io/',
+  base: 'slobich-graphics',
 });
